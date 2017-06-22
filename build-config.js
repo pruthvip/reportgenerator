3008fba038d9ad438c74c3c1999b03b5767f53a2
Pruthvi P <pruthvip15@gmail.com>
2017-06-23 00:35:44 +0530

    Add gh support

--- build-config.js
+++ build-config.js
@@ -1,15 +1,27 @@
 let isWatchEnabled = false;
-const ENV = process.env.NODE_ENV || 'development'; // eslint-disable-line no-process-env
+let compileForGhPages = false;
+const ENV = process.env.NODE_ENV === 'gh' ? 'production' : process.env.NODE_ENV || 'development'; // eslint-disable-line no-process-env
+
+const path = {
+  src: __dirname + '/src',
+  dist:  __dirname + '/dist'
+};
 
 if (process.argv[2] && process.argv[2] === '-w') {
     isWatchEnabled = true;
 }
 
+if (process.argv[3] && process.argv[3] === 'gh') {
+    compileForGhPages = true;
+}
+
+
+if (compileForGhPages) {
+  path.dist = __dirname + '/docs'
+}
+
 module.exports = {
     ENV: ENV,
     isWatchEnabled: isWatchEnabled,
-    path: {
-        src: __dirname + '/src',
-        dist: __dirname + '/dist'
-    }
+    path: path
 };
